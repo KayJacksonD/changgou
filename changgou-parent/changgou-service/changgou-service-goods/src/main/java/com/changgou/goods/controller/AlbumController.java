281b949c5c177f44de9c8adcc50d8d3eff7082f6
Kay <Kay.com>
2019-10-19 15:46:09 +0800

    1019

---
+++ changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/goods/controller/AlbumController.java
@@ -0,0 +1,28 @@
+package com.changgou.goods.controller;
+
+
+import com.changgou.goods.pojo.Album;
+import com.changgou.goods.service.AlbumService;
+import com.github.pagehelper.PageInfo;
+import entity.Result;
+import entity.StatusCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/album")
+public class AlbumController {
+    @Autowired
+    AlbumService albumService;
+
+    @PostMapping("/search/{curPage}/{size}")
+    public Result findPageByCondition(@RequestBody(required = false) Album album,
+                                      @PathVariable("curPage") Integer curPage,
+                                      @PathVariable("size") Integer size) {
+        PageInfo<Album> pageByCondition = albumService.findPageByCondition(album, curPage, size);
+        if (pageByCondition.getList() != null) {
+            return new Result(true, StatusCode.OK, "条件查询相册成功！", pageByCondition);
+        }
+        return new Result(false, StatusCode.ERROR, "条件查询相册为空！");
+    }
+}
---
+++ changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/goods/service/impl/AlbumServiceImpl.java
@@ -0,0 +1,41 @@
+package com.changgou.goods.service.impl;
+
+import com.changgou.goods.dao.AlbumMapper;
+import com.changgou.goods.pojo.Album;
+import com.changgou.goods.service.AlbumService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
+
+@Service
+public class AlbumServiceImpl implements AlbumService {
+    @Autowired(required = false)
+    private AlbumMapper albumMapper;
+
+    @Override
+    public PageInfo<Album> findPageByCondition(Album album, int curPage, int size) {
+        PageHelper.startPage(curPage, size);
+        Example example = new Example(Album.class);
+        Example.Criteria criteria = example.createCriteria();
+        if (album != null) {
+            if (!StringUtils.isEmpty(album.getId())) {
+                criteria.andEqualTo("id", album.getId());
+            }
+            if (!StringUtils.isEmpty(album.getImage())) {
+                criteria.andEqualTo("image", album.getImage());
+            }
+            if (!StringUtils.isEmpty(album.getTitle())) {
+                criteria.andLike("title", "%" + album.getTitle() + "%");
+            }
+            if (!StringUtils.isEmpty(album.getImageItems())) {
+                criteria.andEqualTo("imageItems", album.getImageItems());
+            }
+        }
+        return new PageInfo<Album>(albumMapper.selectByExample(example));
+    }
+}
